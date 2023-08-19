ba9380318d00bdf8edfe0be1f7a84e75fcfd54ef
Cláudio Rico <claudiorico81@hotmail.com>
2023-08-18 23:19:15 -0300

    Finalizado

--- stopwatch.js
+++ stopwatch.js
@@ -1,9 +1,67 @@
 class Stopwatch {
-    elapsedTime = 0;
+    #elapsedTimeInSeconds = 0;
+    #intervalId = null;
 
-    start () {}
+    start (callback = () => {}) {
+        this.#intervalId = setInterval(() => {
+            this.#elapsedTimeInSeconds++;
+            callback();
+        }, 1000);
+    }
 
-    stop () {}
+    stop (callback = () => {}) {
+        clearInterval(this.#intervalId);
+        callback();
+    }
 
-    restart () {}
+    restart (callback = () => {}) {
+        this.#elapsedTimeInSeconds = 0;
+        callback();
+    }
+
+    get elapsedTime() {
+        return Stopwatch.formatTime(this.#elapsedTimeInSeconds);
+    }
+
+    static formatTime(timeInSeconds) {
+        const hours = Math.floor(timeInSeconds / 3600);
+        const minutes = Math.floor((timeInSeconds % 3600) / 60 );
+        const seconds = timeInSeconds - hours * 3600 - minutes * 60;
+
+        return `${Stopwatch.zeroPadding(hours)}:${Stopwatch.zeroPadding(minutes)}:${Stopwatch.zeroPadding(seconds)}`;
+
+    }
+
+    static zeroPadding(originalNumber, desiredAmountDigits = 2){
+        let stringNumber = String(originalNumber);
+        const zeroesRequired = desiredAmountDigits - stringNumber.length;
+
+        if (zeroesRequired <= 0){
+            return stringNumber;
+        }
+
+        for (let counter = 0; counter < zeroesRequired; counter++){
+            stringNumber = `0${stringNumber}`;
+        }
+
+        return stringNumber;
+    }
 }
+
+
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
+const stopwatchDisplay = document.getElementById('stopwatch-display');
+
+function updateDisplay() {
+    stopwatchDisplay.innerText = sw1.elapsedTime;
+}
+
+const sw1 = new Stopwatch();
+
+startBtn.addEventListener('click', () => { sw1.start(updateDisplay) });
+stopBtn.addEventListener('click', () => { sw1.stop(updateDisplay) });
+resetBtn.addEventListener('click', () => { sw1.restart(updateDisplay) });
+
+
